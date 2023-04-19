15c8b0319c96fa685485aff738ef06c7c31c5cde
Brian Salvador <brian.herom@proton.me>
2023-04-19 02:03:32 -0400

    feat(tests): test are created and apply linter complience

--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -1,30 +1,43 @@
-import json
+"""
+Translation module for english and french
+"""
+import os
 from ibm_watson import LanguageTranslatorV3
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
-import os
 from dotenv import load_dotenv
 
 load_dotenv()
 
 apikey = os.environ['apikey']
 url = os.environ['url']
-version = '2023-04-18'
+VERSION = '2023-04-18'
 
 authenticator = IAMAuthenticator(f'{apikey}')
 language_translator = LanguageTranslatorV3(
-    version=f'{version}',
+    version=f'{VERSION}',
     authenticator=authenticator
 )
 
 language_translator.set_service_url(f'{url}')
 
-def englishToFrench(englishText):
-    translator_response = language_translator.translate(text=englishText, model_id='en-fr').get_result()
+def english_to_french(english_text):
+    """
+    function that returns the given text from english to french
+    """
+    if english_text is None:
+        return ""
+    translator_response = language_translator.translate(
+        text=english_text,
+        model_id='en-fr').get_result()
     return translator_response["translations"][0]["translation"]
 
-def frenchToEnglish(frenchText):
-    translator_response = language_translator.translate(text=frenchText, model_id='fr-en').get_result()
+def french_to_english(french_text):
+    """
+    function that returns the given text from french to english
+    """
+    if french_text is None:
+        return ""
+    translator_response = language_translator.translate(
+        text=french_text,
+        model_id='fr-en').get_result()
     return translator_response["translations"][0]["translation"]
-  
-print(englishToFrench('Hello, how are you today?'))
-print(frenchToEnglish("Bonjour, comment vous êtes aujourd'hui?"))
